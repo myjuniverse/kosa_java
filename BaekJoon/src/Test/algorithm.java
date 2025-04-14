b39ee560611fd2d38f3733bfc446ceb5926157d4
Jun Park <rksekrh10@naver.com>
2025-04-14 15:45:09 +0900

    commit

--- BaekJoon/src/Test/algorithm.java
+++ BaekJoon/src/Test/algorithm.java
@@ -1,6 +1,7 @@
 package Test;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class algorithm {
@@ -417,6 +418,33 @@ public class algorithm {
 //		    }
 //		}
 /* ---------------------------------------------------------------------------- */
+//		# baekjoon 9506
+//		Scanner sc = new Scanner(System.in);
+//		while (true) {
+//			int n = sc.nextInt();
+//			if (n == -1) break;
+//			
+//			int sum = 0;
+//			List<Integer> list = new ArrayList<>();
+//			
+//			for (int i = 1; i < n; i++) {
+//				if (n % i == 0) {
+//					sum += i;
+//					list.add(i);
+//				}	
+//			}
+//			if (sum == n) { // 완전수 일때
+//				System.out.print(n + " = ");
+//				for (int i = 0; i < list.size(); i++) {
+//		            System.out.print(list.get(i));
+//		            if (i != list.size() - 1) System.out.print(" + ");
+//		        }
+//		        System.out.println();
+//				
+//			} else { // 완전수가 아님
+//				System.out.println(n + " is NOT perfect.");
+//			}
+//		} // while
 /* ---------------------------------------------------------------------------- */
 /* ---------------------------------------------------------------------------- */
 //		# baekjoon 10818
---
+++ day16_network/src/ex07/network/udp/DatagramServer.java
@@ -0,0 +1,55 @@
+package ex07.network.udp;
+
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class DatagramServer {
+	public static void main(String[] args) {
+		DatagramPacket dp = null;
+		DatagramSocket ds = null;
+		DataOutputStream dos = null;
+		
+		int port = 5000;
+		String str;
+		File file = null;
+		
+		try {
+			System.out.println("-- UDP File Server --");
+			ds = new DatagramSocket(port);
+			
+			while (true) {
+				dp = new DatagramPacket(new byte[65536], 65536);
+				ds.receive(dp); // 데이터 수신
+				
+				str = new String(dp.getData(), 0, dp.getLength()).trim();
+				
+				if (str.equalsIgnoreCase("start")) {
+					System.out.println("전송되고 있습니다...");
+					file = new File("test.txt");
+					
+					dos = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file)));			
+				} else if (str.equalsIgnoreCase("end")) {
+					System.out.println("end");
+					break;
+				} else {
+					System.out.println(str);
+					dos.write(dp.getData(), 0, dp.getLength());
+				} // end if
+			} // end while
+						
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				dos.close();
+			} catch (IOException e) {				
+				e.printStackTrace();
+			}
+		}
+	}
+}
